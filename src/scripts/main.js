c8be5d6032e12101b0fc722d0480016c2656f505
Olga Wukelan <olga.wukelan@gmail.com>
2026-01-08 20:11:28 +0400

    create a file!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+function clickOnLogo() {
+  const promise = new Promise((resolve, reject) => {
+    logo.addEventListener(
+      'click',
+      () => {
+        setTimeout(timer);
+        resolve('Promise was resolved!');
+      },
+      { once: true },
+    );
+
+    const timer = setTimeout(() => {
+      reject(new Error('Promise was rejected!'));
+    }, 3000);
+  });
+
+  promise.then((message) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = message;
+    body.append(div);
+  });
+
+  promise.catch((error) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.textContent = error;
+    body.append(div);
+  });
+}
+
+clickOnLogo();
